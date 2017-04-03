9ed74f976bc756ff751a8c4453d63b53b6993a75
Duncan Hall <duncan.hall@students.olin.edu>
2017-04-03 16:57:36 -0400

    interfaces for iMaster and iReducer added

--- src/iMapper.java
+++ src/iMapper.java
@@ -1,5 +1,11 @@
+import java.rmi.AlreadyBoundException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
 /**
  * Created by duncan on 4/3/17.
  */
-public interface iMapper {
+public interface iMapper extends Remote {
+    public iMapper createMapTask (String name) throws RemoteException, AlreadyBoundException;
+    public void processInput (String input, iMaster theMaster) throws RemoteException, AlreadyBoundException;
 }
--- src/iReducer.java
+++ src/iReducer.java
@@ -1,5 +1,12 @@
+import java.rmi.AlreadyBoundException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
 /**
  * Created by duncan on 4/3/17.
  */
-public interface iReducer {
+public interface iReducer extends Remote {
+    public iReducer createReduceTask (String key, iMaster master) throws RemoteException, AlreadyBoundException;
+    public void receiveValues (int value) throws RemoteException;
+    public int terminate() throws RemoteException;
 }
