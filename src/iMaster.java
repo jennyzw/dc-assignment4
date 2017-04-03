24d5c5c7f2a59a025d52e224eba42bb0ffe48496
Jenny <jennyzw@gmail.com>
2017-04-03 16:58:21 -0400

    iMaster interface

--- src/iMaster.java
+++ src/iMaster.java
@@ -1,6 +1,12 @@
+import java.rmi.*;
+import java.rmi.server.*;
+import java.rmi.RemoteException;
+import java.util.*;
 /**
  * Created by duncan on 4/3/17.
  */
-public interface iMaster {
-
+public interface iMaster extends Remote {
+	public iReducer[] getReducers(String[] keys) throws RemoteException, AlreadyBoundException;
+	public void markMapperDone() throws RemoteException;
+	public void receiveOutput(String key,int value) throws RemoteException;
 }
