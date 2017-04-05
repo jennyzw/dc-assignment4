41df63df5626010b696fdc4b2eb2fab05e03dff5
Duncan Hall <duncan.hall@students.olin.edu>
2017-04-05 17:49:34 -0400

    seperated MapperReducer code into MapTask and MapReducer

---
+++ src/MapTask.java
@@ -0,0 +1,63 @@
+import java.rmi.AlreadyBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
+
+/**
+ * Created by duncan on 4/3/17.
+ */
+
+public class MapTask implements iMapper {
+
+    public MapTask(boolean isManager) {
+
+        if (isManager) {
+            // export manager objects and bind to local rmi for master access
+            try {
+                Registry registry = LocateRegistry.getRegistry();
+                iMapper mapManagerStub = (iMapper) UnicastRemoteObject.exportObject(this);
+                registry.rebind("mapManager", mapManagerStub);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    @Override
+    public iMapper createMapTask(String name) throws RemoteException, AlreadyBoundException {
+        return (iMapper) UnicastRemoteObject.exportObject(new MapperReducer(false), 0);
+    }
+
+    @Override
+    public void processInput(String input, iMaster theMaster) throws RemoteException, AlreadyBoundException {
+        String [] words = input.split("\\s+");
+        HashMap<String, Integer> miniHist = new HashMap<>();
+
+        for (String word : words) {
+            word = word.replace("[^a-zA-Z]", "").toLowerCase();
+            miniHist.put(word, miniHist.getOrDefault(word, 0) + 1);
+        }
+
+        String[] distinctWords = (String[]) miniHist.keySet().toArray();
+
+        iReducer[] reducers = theMaster.getReducers(distinctWords);
+
+        for (int i = 0; i < distinctWords.length; i++) {
+            final int finalI = i;
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        reducers[finalI].receiveValues(miniHist.get(distinctWords[finalI]));
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+
+    }
+}
---
+++ src/ReduceTask.java
@@ -0,0 +1,42 @@
+import java.rmi.AlreadyBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+/**
+ * Created by duncan on 4/3/17.
+ */
+public class ReduceTask implements iReducer {
+
+    public ReduceTask(boolean isManager) {
+
+        if (isManager) {
+            // export manager objects and bind to local rmi for master access
+            try {
+                Registry registry = LocateRegistry.getRegistry();
+                iReducer reduceManagerStub = (iReducer) UnicastRemoteObject.exportObject(this);
+                registry.rebind("reduceManager", reduceManagerStub);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    @Override
+    public iReducer createReduceTask(String key, iMaster master) throws RemoteException, AlreadyBoundException {
+        return null;
+    }
+
+    @Override
+    public void receiveValues(int value) throws RemoteException {
+
+    }
+
+    @Override
+    public int terminate() throws RemoteException {
+        return 0;
+    }
+
+}
