97364ced03314522e4fb228e8ecb106619481ff1
Jenny <jennyzw@gmail.com>
2017-04-22 13:28:51 -0400

    more

--- src/Manager.java
+++ src/Manager.java
@@ -2,6 +2,7 @@
  * Created by duncan on 4/6/17.
  */
 public class Manager implements iMapperManager, iReducerManager {
+
     public static void main(String[] args) {
         new MapTask("mapManager", true);
         new ReduceTask(true);
