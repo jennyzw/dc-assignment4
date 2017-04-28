cdb1731ef8e8b47620ebd222ee8649522ade0568
Duncan Hall <duncan.hall@students.olin.edu>
2017-04-28 14:34:33 -0400

    stuff

--- src/Manager.java
+++ src/Manager.java
@@ -1,3 +1,6 @@
+import java.rmi.AlreadyBoundException;
+import java.rmi.RemoteException;
+
 /**
  * Created by duncan on 4/6/17.
  */
@@ -6,6 +9,31 @@ public class Manager implements iMapperManager, iReducerManager {
     public static void main(String[] args) {
         new MapTask("mapManager", true);
         new ReduceTask(true);
+    }
+
+    @Override
+    public int createMapTask(String name) throws RemoteException, AlreadyBoundException {
+        return 0;
+    }
+
+    @Override
+    public void processInput(int id, String input, iMaster theMaster) throws RemoteException, AlreadyBoundException {
+
+    }
+
+    @Override
+    public int createReduceTask(String key, iMaster master) throws RemoteException, AlreadyBoundException {
+        return 0;
+    }
+
+    @Override
+    public void receiveValues(int id, int value) throws RemoteException {
+
+    }
+
+    @Override
+    public int terminate() throws RemoteException {
+        return 0;
     }
 }
 
--- src/iMaster.java
+++ src/iMaster.java
@@ -2,13 +2,12 @@ import java.io.Serializable;
 import java.rmi.AlreadyBoundException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-import java.util.ArrayList;
 
 /**
  * Created by duncan on 4/3/17.
  */
 public interface iMaster extends Remote,Serializable {
-	public Tuple<iReduceManager[], int[]> getReducers(String[] keys) throws RemoteException, AlreadyBoundException;
+	public Tuple<iReducerManager, Integer>[] getReducers(String[] keys) throws RemoteException, AlreadyBoundException;
 	public void markMapperDone(int id, iMapperManager manager) throws RemoteException;
 	public void receiveOutput(String key,int value) throws RemoteException;
 }
