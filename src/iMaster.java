dd948e26b6b7aeaaa5d97031acbdebebaad8b31f
Duncan Hall <duncan.hall@students.olin.edu>
2017-04-28 14:36:35 -0400

    t

--- src/iMaster.java
+++ src/iMaster.java
@@ -6,7 +6,7 @@ import java.rmi.RemoteException;
 /**
  * Created by duncan on 4/3/17.
  */
-public interface iMaster extends Remote,Serializable {
+public interface iMaster extends Remote, Serializable {
 	public Tuple<iReducerManager, Integer>[] getReducers(String[] keys) throws RemoteException, AlreadyBoundException;
 	public void markMapperDone(int id, iMapperManager manager) throws RemoteException;
 	public void receiveOutput(String key,int value) throws RemoteException;
